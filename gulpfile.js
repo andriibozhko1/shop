8f2c5d5dc385cee1edf7f3565358de80b9fcc2a5
Andrii <andriibozhko1@gmail.com>
2019-01-31 12:49:25 +0200

    code-review

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,8 @@ const stylesBlob = 'src/scss/**';
 const sassBlob = 'src/scss/**';
 const jsBlob = 'src/js/**';
 const normalizeBlob = 'src/normalize/**';
+const components = 'src/js/components/**';
+const api = 'src/api/**';
 
 gulp.task('default', function () {
   return runSequence('build', 'serve');
@@ -21,7 +23,7 @@ gulp.task('default', function () {
 gulp.task('build', function () {
   return runSequence(
     'cleanDist',
-    ['processStyles', 'processHtml', 'processImages','sass','js','normalize']
+    ['processStyles', 'processHtml', 'processImages','sass','js','normalize','components','api']
   );
 });
 
@@ -96,4 +98,12 @@ gulp.task('js', function() {
 gulp.task('normalize', function() {
   return gulp.src(normalizeBlob)
   .pipe(gulp.dest(`${distDirectory}/normalize/`));
+});
+gulp.task('components', function() {
+  return gulp.src(components)
+  .pipe(gulp.dest(`${distDirectory}/js/`));
+});
+gulp.task('api', function() {
+  return gulp.src(api)
+  .pipe(gulp.dest(`${distDirectory}/api`));
 });
