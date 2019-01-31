db745874473a4b9223e0805b35c9f9a8730b7633
Andrii <andriibozhko1@gmail.com>
2019-01-31 12:55:47 +0200

    code-review

---
+++ dist/js/components/Cart/cart.js
@@ -0,0 +1,72 @@
+export default class Cart {
+  constructor({element, phonesList}) {
+    this.element = element;
+    this.phonesList = phonesList;
+    this.phones = [];    
+
+    this.phonesList.map(e => e.quantity = 1);
+
+    this.addEvents();    
+  }
+  render() {    
+    this.phones = this.phones.filter((a,b) => {
+      if(this.phones.indexOf(a) == b) {
+        return true;
+      } else {
+        a.quantity++;
+      }
+    });
+      
+    let phones = this.phones.map((phone) => {
+      return `
+         <div class="cart-menu__item" data-product-id="${phone.id}">
+            <div class="cart-menu__remove-item">
+              <img src="img/icons/error.svg" alt="cross" class="cart-menu__remove-item-icons">
+            </div>
+            <div class="cart-menu__item-img-block">
+              <img src="${phone.imageUrl}" alt="${phone.id}" class="cart-menu__item-img">
+            </div>
+            <div class="cart-menu__item-title">
+              ${phone.name}
+            </div>
+            <div class="cart-menu__quantity">
+            ${phone.quantity}
+            </div>
+        </div>
+      `
+    }).join("");    
+
+    this.element.innerHTML = phones;
+  }
+
+  addEvents() {
+    document.body.addEventListener('click', (e) => {
+      if(e.target.dataset.toOrderId) {        
+        this.phonesList.map((phone) => {
+          if(phone.id === e.target.dataset.toOrderId) {
+            this.phones.push(phone);
+          }
+        })
+        this.render();
+        this.element.closest('.cart-menu').classList.remove('hide');
+      }
+    })
+    
+    this.element.addEventListener('click', (e) => {
+      if(e.target.closest('.cart-menu__remove-item')) {
+        let item = e.target.closest('.cart-menu__item');
+
+        this.phones.map((phone) => {
+          if(phone.id === item.dataset.productId) {
+            if(phone.quantity > 1) {
+              phone.quantity--;                      
+            } else {                            
+              this.phones.splice(this.phones.indexOf(phone),1);                            
+            }            
+            this.render();
+          }
+        })
+      }
+    })
+  }
+}
---
+++ dist/js/components/Phones/phones-list.js
@@ -0,0 +1,50 @@
+import ViewPhone from "../View Phone/view-phone.js";
+import ServerRequest from "../../server-request/server-request.js";
+
+export default class PhonesList {
+  constructor({ element, phones }) {
+    this.element = element;
+    this.phonesList = phones;
+
+    this.render();
+    this.addEvents();
+  }
+  render() {
+    let phones = this.phonesList
+      .map(e => {
+        return `
+          <div class="product" data-product-id="${e.id}">
+            <div class="product__photo">
+                <img src="${e.imageUrl}" alt="${e.id}" class="product__img">
+            </div>
+            <div class="product__title">
+              ${e.name}
+            </div>
+            <div class="product__snippet">
+                  ${e.snippet}
+            </div>
+            <button class="product__to-order" data-to-order-id="${
+              e.id
+            }">Buy</button>
+          </div>      
+      `;
+      })
+      .join("");
+    this.element.innerHTML = phones;
+  }
+  addEvents() {        
+    this.element.addEventListener("click", e => {
+      if (e.target.closest(".product") && !e.target.dataset.toOrderId) {        
+        let productId = e.target.closest(".product").dataset.productId;
+
+        let findPhoneById = new ServerRequest();
+        this.element.classList.add("hide");
+        new ViewPhone({
+          element: document.querySelector(".view-phone"),
+          phone: findPhoneById.findById(productId),
+          phoneList: this.element
+        });
+      }
+    });
+  }
+}
